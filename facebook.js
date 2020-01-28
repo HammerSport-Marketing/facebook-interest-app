5d30435907ba502a22a519d3751c9bfd113d70bd
Riley Seaburg <riley@rds.marketing>
2020-01-27 20:01:58 -0500

    console.log

--- facebook.js
+++ facebook.js
@@ -34,31 +34,51 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-function checkLoginState() {
-    FB.getLoginStatus(function (response) {
-        if (response.status === 'connected') {
-            // The user is logged in and has authenticated your
-            // app, and response.authResponse supplies
-            // the user's ID, a valid access token, a signed
-            // request, and the time the access token 
-            // and signed request each expire.
-            var uid = response.authResponse.userID;
-            var accessToken = response.authResponse.accessToken;
-            // Add to LS
-            Store.addToken(accessToken);
-        }
-        else if (response.status === 'not_authorized') {
-            // The user hasn't authorized your application.  They
-            // must click the Login button, or you must call FB.login
-            // in response to a user gesture, to launch a login dialog.
-        }
-        else {
-            // The user isn't logged in to Facebook. You can launch a
-            // login dialog with a user gesture, but the user may have
-            // to log in to Facebook before authorizing your application.
-        }
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: "1033093773710625",
+        cookie: true,
+        xfbml: true,
+        version: "v5.0"
     });
-}
+    FB.AppEvents.logPageView();
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+    function checkLoginState() {
+        FB.getLoginStatus(function (response) {
+            if (response.status === 'connected') {
+                // The user is logged in and has authenticated your
+                // app, and response.authResponse supplies
+                // the user's ID, a valid access token, a signed
+                // request, and the time the access token 
+                // and signed request each expire.
+                var uid = response.authResponse.userID;
+                var accessToken = response.authResponse.accessToken;
+                console.log(accessToken);
+                // Add to LS
+                Store.addToken(accessToken);
+            }
+            else if (response.status === 'not_authorized') {
+                // The user hasn't authorized your application.  They
+                // must click the Login button, or you must call FB.login
+                // in response to a user gesture, to launch a login dialog.
+            }
+            else {
+                // The user isn't logged in to Facebook. You can launch a
+                // login dialog with a user gesture, but the user may have
+                // to log in to Facebook before authorizing your application.
+            }
+        });
+    }
+};
 var FBLogin = document.getElementById('FBLogin');
 // Local Storage
 var Store = /** @class */ (function () {
