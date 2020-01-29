ba60b2a7ffd374aab2d77a9d54085d28c099533c
Riley Seaburg <riley@rds.marketing>
2020-01-29 13:29:16 -0500

    remove json

--- facebook.js
+++ facebook.js
@@ -73,7 +73,7 @@ var Facebook = /** @class */ (function () {
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0:
-                        token = JSON.parse(localStorage.getItem('token'));
+                        token = localStorage.getItem('token');
                         return [4 /*yield*/, fetch("https://graph.facebook.com/search?type=adinterest&q=" + search + "&limit=10000&locale=en_US&access_token=" + token)];
                     case 1:
                         searchResponse = _a.sent();
--- facebook.ts
+++ facebook.ts
@@ -37,7 +37,7 @@ class Store {
 class Facebook {
 
   async getSearchData(search) {
-    const token = JSON.parse(localStorage.getItem('token'));
+    const token = localStorage.getItem('token');
     const searchResponse = await fetch(`https://graph.facebook.com/search?type=adinterest&q=${search}&limit=10000&locale=en_US&access_token=${token}`);
 
     const responseData = await searchResponse[0].json();
