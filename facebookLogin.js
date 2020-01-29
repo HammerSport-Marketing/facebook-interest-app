c274453907bcf9f5e2e6b6e21cd6aafdbb97ae32
Riley Seaburg <riley@rds.marketing>
2020-01-29 13:26:10 -0500

    SDK & structure

--- facebookLogin.js
+++ facebookLogin.js
@@ -1,19 +1,49 @@
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response); // The current login status of the person.
+    if (response.status === 'connected') { // Logged into your webpage and Facebook.
+        localStorage.setItem('token', response.authResponse.accessToken);
+        {
+            console.log('itemset');
+        }
+        ;
+        testAPI();
+    }
+    else { // Not logged into your webpage or we are unable to tell.
+        document.getElementById('status').innerHTML = 'Please log ' +
+            'into this webpage.';
+    }
+}
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+}
 window.fbAsyncInit = function () {
     FB.init({
         appId: '370460146853007',
         cookie: true,
         xfbml: true,
-        version: '{5.0}'
+        version: 'v5.0' // Use this Graph API version for this call.
     });
-    FB.AppEvents.logPageView();
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response); // Returns the login status.
+    });
 };
 (function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
+    if (d.getElementById(id))
         return;
-    }
     js = d.createElement(s);
     js.id = id;
     js.src = "https://connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function (response) {
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+    });
+}
